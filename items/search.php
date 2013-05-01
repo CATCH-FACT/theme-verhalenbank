478aad9ced83ee9d05b7b74e08bf7b578260a8db
Iwe Muiser <e.c.muiser@gmail.com>
2013-05-01 13:01:09 +0200

    more sense in the search nav

--- items/search.php
+++ items/search.php
@@ -10,31 +10,28 @@ echo head(array('title' => $pageTitle,
 if (!empty($_GET['style'])) {
     $search_style = $_GET['style'];
 } else {
-    $search_style = "medium";
+    $search_style = "advanced";
 }
 ?>
 
+
 <nav class="items-nav navigation secondary-nav">
-    <ul class="navigation">
-        <li>
-            <a href="search?style=medium"><?php echo __("Advanced Search")?></a>
+    <ul id="section-nav" class="navigation">
+        <li class="<?php if (isset($_GET['style']) &&  $_GET['style'] == 'advanced') {echo 'navigation_current';} ?>">
+            <a href="<?php echo html_escape(url('items/search?style=advanced')); ?>"><?php echo __('Advanced Search'); ?></a>
         </li>
-        <li>
-            <a href="search?style=advanced"><?php echo __("Very Advanced Search")?></a>
+        <li class="<?php if (isset($_GET['style']) && $_GET['style'] == 'veryadvanced') {echo 'navigation_current';} ?>">
+            <a href="<?php echo html_escape(url('items/search?style=veryadvanced')); ?>"><?php echo __('VERY advanced Search'); ?></a>
         </li>
     </ul>
 </nav>
 
-<!--<nav class="items-nav navigation secondary-nav"> -->
-    <?php #echo public_nav_items(); ?>
-<!--</nav> -->
-
 <?php 
-if ($search_style == "medium"){
+if ($search_style == "advanced"){
     echo $this->partial('items/search-form-medium.php', array('formAttributes' =>
                         array('id'=>'advanced-search-form')));
 }
-elseif ($search_style == "advanced"){
+elseif ($search_style == "veryadvanced"){
     echo $this->partial('items/search-form.php', array('formAttributes' =>
                         array('id'=>'advanced-search-form')));
 }?>
