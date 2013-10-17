0ed261ae39af5b4fc261a39108a26588e563d42c
Iwe Muiser <e.c.muiser@gmail.com>
2013-10-17 17:01:35 +0200

    trefwoorden fix

--- items/tags.php
+++ items/tags.php
@@ -1,6 +1,7 @@
 <?php
 $pageTitle = __('Browse Items');
 echo head(array('title'=>$pageTitle, 'bodyclass'=>'items tags'));
+
 ?>
 
 <h1><?php echo $pageTitle; ?></h1>
@@ -8,7 +9,28 @@ echo head(array('title'=>$pageTitle, 'bodyclass'=>'items tags'));
 <nav class="navigation items-nav secondary-nav">
     <?php echo public_nav_items(); ?>
 </nav>
+<br>
+<h3> Top 100 </h3>
 
-<?php echo tag_cloud($tags, 'items/browse'); ?>
+<?php 
+    $most_tags = get_records('Tag',
+                        array(
+                             'sort_field'=>'count',
+                             'sort_dir'=>'d'),
+                        100);
+    echo tag_cloud($most_tags, 'items/browse', $maxClasses = 15);
+    
+    if (!empty($_GET['sort_field'])) {
+        print "<h3> Alphabetisch </h3>";
+        $alpha_tags = get_records('Tag',
+                            array('sort_field'=>'name',
+                                  'sort_dir'=>'a'),
+                                  100000);
+        echo tag_cloud($alpha_tags, 'items/browse', $maxClasses = 15);
+    } else { ?>
+            <br>
+            <a href="<?php echo html_escape(url('items/tags?sort_field=name')); ?>"><?php echo __('Alle trefwoorden alphabetisch weergeven (laden duurt lang)'); ?></a>
+            <br>
+    <?php } ?>
 
 <?php echo foot(); ?>
